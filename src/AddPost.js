e891a8a1597c3cc434846f13236fc97959e2e661
Johny Thomas <johnythomas007@gmail.com>
2018-08-27 23:36:38 +0530

    Feat: Add Header for AddPost page UI

---
+++ src/AddPost.js
@@ -0,0 +1,6 @@
+import React from "react"
+import Header from "./Header"
+
+const AddPost = () => <Header />
+
+export default AddPost
--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react"
 import { Route, Switch } from "react-router-dom"
 import ListPostsPage from "./ListPostPage"
 import PostDetails from "./PostDetails"
+import AddPost from "./AddPost"
 
 const App = () => (
   <Switch>
     <Route exact path="/" component={ListPostsPage} />
     <Route path="/post/:id" component={PostDetails} />
+    <Route path="/addPost" component={AddPost} />
   </Switch>
 )
 
