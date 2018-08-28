bafa8905f64e9cb038872b4b048e64b4824ad928
Johny Thomas <johnythomas007@gmail.com>
2018-08-28 22:46:34 +0530

    Feat: Add NotFound Page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Route, Switch } from "react-router-dom"
 import ListPostsPage from "./ListPostPage"
 import PostDetails from "./PostDetails"
 import AddPost from "./AddPost"
+import NotFound from "./NotFound"
 
 const App = () => (
   <Switch>
     <Route exact path="/" component={ListPostsPage} />
     <Route path="/post/:id" component={PostDetails} />
     <Route path="/addPost" component={AddPost} />
+    <Route component={NotFound} />
   </Switch>
 )
 
---
+++ src/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Grid, Typography } from "@material-ui/core"
+import Header from "./Header"
+
+const NotFound = () => (
+  <Grid>
+    <Header />
+    <Typography>404 Page Not found...</Typography>
+  </Grid>
+)
+
+export default NotFound
