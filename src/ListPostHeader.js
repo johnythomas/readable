821f65c53600ed44bcd2e9664db0ea096f12061b
Johny Thomas <johnythomas007@gmail.com>
2018-10-26 22:16:30 +0530

    Refactor: Make ListPostHeader stateful

--- src/ListPostHeader.js
+++ src/ListPostHeader.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { Component } from "react"
 import { Link } from "react-router-dom"
 import {
   Grid,
@@ -36,64 +36,82 @@ const styles = theme => ({
   }
 })
 
-const ListPostHeader = ({ classes }) => (
-  <Grid
-    className={classes.navBarContainer}
-    container
-    justify="center"
-    alignItems="center"
-  >
-    <Grid item md={6}>
-      <Grid container>
-        <Grid item md={10}>
-          <Typography className={classes.whiteText} variant="display1">
-            Readable
-          </Typography>
-        </Grid>
-        <Grid
-          className={classes.addPostLink}
-          component={Link}
-          to="/addPost"
-          item
-          md={2}
-          container
-          justify="flex-end"
-          alignItems="flex-end"
-        >
-          <Grid
-            className={classes.addPostIcon}
-            container
-            item
-            md={6}
-            justify="flex-end"
-          >
-            <AddComment />
+class ListPostHeader extends Component {
+  state = {
+    query: ""
+  }
+
+  handleQueryChange = e => {
+    const { value } = e.target
+    this.setState(() => ({
+      query: value
+    }))
+  }
+
+  render() {
+    const { classes } = this.props
+    return (
+      <Grid
+        className={classes.navBarContainer}
+        container
+        justify="center"
+        alignItems="center"
+      >
+        <Grid item md={6}>
+          <Grid container>
+            <Grid item md={10}>
+              <Typography className={classes.whiteText} variant="display1">
+                Readable
+              </Typography>
+            </Grid>
+            <Grid
+              className={classes.addPostLink}
+              component={Link}
+              to="/addPost"
+              item
+              md={2}
+              container
+              justify="flex-end"
+              alignItems="flex-end"
+            >
+              <Grid
+                className={classes.addPostIcon}
+                container
+                item
+                md={6}
+                justify="flex-end"
+              >
+                <AddComment />
+              </Grid>
+              <Grid item md={6}>
+                <Typography className={classes.whiteText} variant="title">
+                  Post
+                </Typography>
+              </Grid>
+            </Grid>
           </Grid>
-          <Grid item md={6}>
-            <Typography className={classes.whiteText} variant="title">
-              Post
-            </Typography>
+          <Grid container className={classes.divider}>
+            <Grid item md={12}>
+              <FormControl fullWidth>
+                <Input
+                  className={classes.searchText}
+                  value={this.state.query}
+                  startAdornment={
+                    <InputAdornment position="start">
+                      <Search color="primary" />
+                    </InputAdornment>
+                  }
+                  disableUnderline
+                  placeholder="Search for your post..."
+                  onChange={this.handleQueryChange}
+                />
+              </FormControl>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
-      <Grid container className={classes.divider}>
-        <Grid item md={12}>
-          <FormControl fullWidth>
-            <Input
-              className={classes.searchText}
-              startAdornment={
-                <InputAdornment position="start">
-                  <Search color="primary" />
-                </InputAdornment>
-              }
-              disableUnderline
-              placeholder="Search for your post..."
-            />
-          </FormControl>
-        </Grid>
-      </Grid>
-    </Grid>
-  </Grid>
-)
+    )
+  }
+}
 
 export default withStyles(styles)(ListPostHeader)
